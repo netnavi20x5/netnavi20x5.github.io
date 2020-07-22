cceaa8ecd59e2dd19b6b3078b0d04d0fe58026aa
netnavi20x5 <warpfactorsix@gmail.com>
2020-07-22 21:43:14 +0800

    dsadasd
    asdasdasdsad

--- Yume Mk 3/360/Bangsar/js/main.js
+++ Yume Mk 3/360/Bangsar/js/main.js
@@ -25,7 +25,7 @@ var camera,
         width = 1024, // int || window.innerWidth || default 4:3
         height = 768, // int || window.innerHeight || default 4:3
         ratio = width / height;
-var texture = THREE.ImageUtils.loadTexture('img/Demo/R0010042.JPG', new THREE.UVMapping(), function() {
+var texture = THREE.ImageUtils.loadTexture('img/TestImg.JPG', new THREE.UVMapping(), function() {
     init();
     animate();
 });
